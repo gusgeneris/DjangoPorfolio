e9b5753603693351c5577ea03bb3bc8d5db6162d
gusgeneris <gusgeneris92@gmail.com>
2022-03-17 18:01:07 -0300

    Estilizacion de la appBlog y rediseño del modelo de la appBlog

--- blogApp/models.py
+++ blogApp/models.py
@@ -1,10 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    imagen = models.ImageField(upload_to='blog/imagenes')
+    
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+    
+    def __str__(self):
+        return self.nombre
+  
+
 class Post(models.Model):
     titulo = models.CharField(max_length=100)
     descripcion = models.TextField()
     imagen = models.ImageField(upload_to='blog/imagenes')
     date = models.DateTimeField(auto_now_add=True)
+    categorias = models.ManyToManyField(Categoria, null = 'True')
+    
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+        
+    def __str__(self):
+        return self.titulo
     
     
