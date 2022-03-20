1bbbb5feac482b5ef8e712806dad737895ebb5da
gusgeneris <gusgeneris92@gmail.com>
2022-03-19 22:34:35 -0300

    heroku

--- DjangoPorfolio/settings/dev.py
+++ DjangoPorfolio/settings/dev.py
@@ -9,11 +9,11 @@ ALLOWED_HOSTS = ['*']
 
 STATIC_URL = 'static/'
 
-#STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'staticfiles'),
+#)
 
 MEDIA_URL = 'media/'
 
