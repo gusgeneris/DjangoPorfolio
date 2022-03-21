3795a2843142cf3c8be4bf3f43cc0f8a4fe0b4a2
gusgeneris <gusgeneris92@gmail.com>
2022-03-21 03:44:56 -0300

    Static files

--- DjangoPorfolio/settings/prod.py
+++ DjangoPorfolio/settings/prod.py
@@ -50,7 +50,7 @@ STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 
 # Heroku Logging
