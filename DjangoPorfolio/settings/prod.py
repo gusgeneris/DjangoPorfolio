87b2248bb34d68c01d5b966e3bb4a24014fbc4b0
gusgeneris <gusgeneris92@gmail.com>
2022-03-21 16:36:21 -0300

    heroku

--- DjangoPorfolio/settings/prod.py
+++ DjangoPorfolio/settings/prod.py
@@ -43,12 +43,14 @@ AWS_HEADERS = {'Access-Control-Allow-Origin': '*'}
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
+AWS_PRELOAD_METADATA = True
 
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
