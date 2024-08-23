1f26852bfb26d1dac6971b36cf2c46217b686649
gusgeneris92 <gusgeneris92@gmail.com>
2024-08-23 19:22:51 -0300

    test

--- DjangoPorfolio/settings/base.py
+++ DjangoPorfolio/settings/base.py
@@ -124,9 +124,9 @@ CKEDITOR_CONFIGS = {
     },
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS= True
-EMAIL_PORT = '587'
-EMAIL_HOST_USER = config('EMAIL_HOST_NAME')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS= True
+# EMAIL_PORT = '587'
+# EMAIL_HOST_USER = config('EMAIL_HOST_NAME')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
