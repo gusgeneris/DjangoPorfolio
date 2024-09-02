bf3966c5e129e58adbac5af131e9c2eb1aa98baf
gusgeneris92 <gusgeneris92@gmail.com>
2024-09-01 21:39:28 -0300

    Actualizacion de politicas AWS

--- DjangoPorfolio/settings/prod.py
+++ DjangoPorfolio/settings/prod.py
@@ -44,7 +44,7 @@ AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-AWS_DEFAULT_ACL = 'public-read'
+# AWS_DEFAULT_ACL = 'public-read'
 
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
